82ab2fc5d52d4ba5dc9c38a252f699f3ca4063ac
hipnarowicz <61248366+hipnarowicz@users.noreply.github.com>
2020-04-05 19:19:46 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,13 @@
 const router = require('koa-router')()
 const store = require('../store')
 
+
+router.get('/', async (ctx, next) => {
+  await ctx.render('index', {
+    title: 'Hello whatever!'
+  })
+})
+
 router.get('/string', async (ctx, next) => {
   tasks = await store.listTasks()
   await ctx.render('index', { tasks })
